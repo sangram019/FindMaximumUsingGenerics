25a8a0580311a6e1b463461ee29ac915a4a93252
Sangram <sangram15.behera@gmail.com>
2022-05-16 04:04:51 +0530

    addd By Sangram

---
+++ FindMaximumUsingGenerics/Generic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumber
+{
+    public class FindMaximum<T> where T : IComparable
+    {
+        public T first, second, third;
+
+        public FindMaximum(T first, T second, T third)
+        {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+        }
+        public static T MaxValueGeneric<T>(T first, T second, T third) where T : IComparable<T>
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine("First Number is greater: " + first);
+                return first;
+            }
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine("Second Number is greater: " + second);
+                return second;
+            }
+            else
+            {
+                Console.WriteLine("Third Number is greater: " + third);
+                return third;
+            }
+            return default;
+        }
+
+
+    }
+}
