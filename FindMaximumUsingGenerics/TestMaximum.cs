a0537f14a8d0aea70164d22b3e1c321e59161d97
Sangram <sangram15.behera@gmail.com>
2022-05-16 04:03:23 +0530

    Added By Sangram

---
+++ FindMaximumUsingGenerics/TestMaximum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumber
+{
+    public class FindMaximumNum<T> where T : IComparable
+    {
+        public T[] value;
+
+        public FindMaximumNum(T[] value)
+        {
+            this.value = value;
+        }
+        public static T[] Sort<T>(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        public static T MaxValue<T>(params T[] values)
+        {
+            var sorted_values = Sort(values);
+            return sorted_values[^1];
+        }
+        public T PrintMaxMethod()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("Maximum value is :" + max);
+            return max;
+        }
+    }
+}
